432f8ad86b6b98ac89d69c057d656484bfd85a05
lijing <lijing@88bank.com>
2018-04-11 18:25:12 +0800

    测试

---
+++ ioc/src/test/java/com/ioc/other/Test.java
@@ -0,0 +1,14 @@
+package test.java.com.ioc.other;
+
+/**
+ * Title: Test
+ * Package: test.java.com.ioc.other
+ * Author: LiJing
+ * CreateTime: 2018年04月11日 18:19
+ * Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello IOC");
+    }
+}
